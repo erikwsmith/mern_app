a54c401de476d577db20154f3f2c619fda34aa7d
erikwsmith <erikwsmith85@gmail.com>
2024-01-28 02:41:29 -0600

    third attempt to correct refresh error

--- server/routes/appRouter.js
+++ server/routes/appRouter.js
@@ -8,13 +8,18 @@ router.get('/', (req, res) => {
     res.status(201).json({message: 'React Web App'});
 })
 
-
-
 //get all records
 router.get('/records', getRecords);
 
 //create record
 router.get('/create_record', createRecord);
 
-
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '.../frontend/public/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+  
 module.exports = router;
