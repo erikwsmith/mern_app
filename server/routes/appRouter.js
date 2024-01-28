96ca45c9e885784f60afb1f425615c1ffcf587ef
erikwsmith <erikwsmith85@gmail.com>
2024-01-28 02:54:15 -0600

    removed router

--- server/routes/appRouter.js
+++ server/routes/appRouter.js
@@ -14,12 +14,4 @@ router.get('/records', getRecords);
 //create record
 router.get('/create_record', createRecord);
 
-router.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, '\\/frontend/public/index.html'), function(err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    })
-  })
-
 module.exports = router;
